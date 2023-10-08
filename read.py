d4ffd9c7135b68ba51bccd000187810927be217a
ashiouzzaman <ashiouzzaman.code@gmail.com>
2023-10-08 16:57:51 +0600

    Read & play video

--- read.py
+++ read.py
@@ -1,11 +1,27 @@
 import cv2 as cv
 
-# Reading image from directory
-img = cv.imread("Photos/cat.jpg")
+# # Reading image from directory
+# img = cv.imread("Photos/cat.jpg")
 
-# Showing Image
-cv.imshow("Pussy Cat", img)
+# # Showing Image
+# cv.imshow("Pussy Cat", img)
+
+# Reading video
+video = cv.VideoCapture('Videos/kitten.mp4')
+
+while True:
+    isTrue, frame = video.read()
+    cv.imshow('Kitten Video', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+video.release()
+cv.destroyAllWindows()
 
 
-cv.waitKey(0)
+
+
+
+# cv.waitKey(0)
 
